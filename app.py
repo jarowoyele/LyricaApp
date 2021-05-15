423aeb95f068f6d3e14199192711c0ac2b2fffcf
jarowoyele <77020047+jarowoyele@users.noreply.github.com>
2021-05-15 11:08:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 from split import unique 
 from azlyrics.azlyrics import lyrics
 # from streamlit.proto.TextArea_pb2 import TextArea
@@ -23,11 +20,9 @@ def main():
         profane = profane_words.replace(",", " ")
         profane = profane.split(" ")
         profane = unique.union(set(profane))
-        stpwrds = stopwords.words("english")
-        tokens = word_tokenize(searchlyrics())
+        stpwrds = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+        tokens = searchlyrics().split(" ")
         removing_stopwords = [words for words in tokens if not words in stpwrds]
-        new_stopwords = stopwords.words("english")
-        new_stopwords = new_stopwords.extend(profane)
         removing = [word for word in removing_stopwords if word in profane]
         return removing
 
@@ -58,10 +53,7 @@ def main():
             if Analyze:
                 with st.beta_expander("Keywords"): 
                      st.write(parse_lyrics())
-
-        
-         
-               
+          
     
     else:
         st.subheader("About")
